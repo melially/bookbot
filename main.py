7e93fa9a3fe0dcd7df979eada27d0b31fd394380
melially <mat.levasseur@outlook.com>
2025-02-21 17:32:30 -0500

    Added wordcounting fxn

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-#get_book_text function; takes file path, returns contents as string
-"""
-with open(path_to_file) as f:
-    # do something with f (the file) here
+def wordcount(source):
+    count = len(source.split())
+    print(f"{count} words found in the document")
 
-To turn into string
-# f is a file object
-file_contents = f.read()
-"""
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
-        print(file_contents)
-
-#main function that uses above w/ /relative/ path to txt to print to console
+        #print(file_contents)
+        wordcount(file_contents)
 
 def main():
     get_book_text("books/frankenstein.txt")
-#call main
-main()
-"""
 
-asdasd
-"""
+
+main()

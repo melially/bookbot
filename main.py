0573f4a5ab9d82a730d7a46cac6c41924c329ea1
melially <mat.levasseur@outlook.com>
2025-02-22 16:16:12 -0500

    Per last commit really

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from stats import get_num_words
 from stats import character_count
+import sys
+print(sys.argv)
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
-        #print(file_contents)
-        #get_num_words(file_contents)
         return file_contents
 
 def main():
-    file = "books/frankenstein.txt"
+    file = sys.argv[1]
+
+    sys.argv
 
     get_book_text(file)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file}...")
     get_num_words(get_book_text(file))
     character_count(file)
-    
-    #print(file_contents)
-    #/ne: 44538
-    #/nt: 29493
     print("============= END ===============")
-#"""
+
 main()
